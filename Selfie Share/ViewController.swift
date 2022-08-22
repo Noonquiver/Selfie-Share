dc20ecb29e6cdb0623a3c68b897a475ed1b5b913
noonquiver <camilo.hernandez25@hotmail.com>
2022-08-22 13:50:50 -0500

    Setting up multimultipeer connectivity.

--- Selfie Share/ViewController.swift
+++ Selfie Share/ViewController.swift
@@ -5,18 +5,40 @@
 //  Created by Camilo Hernández Guerrero on 22/08/22.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     var images = [UIImage]()
+    
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssistant: MCAdvertiserAssistant?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Selfie Share"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self
     }
     
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "Selfie Share", discoveryInfo: nil, session: mcSession)
+    }
+    
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "Selfie Share", session: mcSession)
+        
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
@@ -44,5 +66,13 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         
         images.insert(image, at: 0)
         collectionView.reloadData()
+    }
+    
+    @objc func showConnectionPrompt() {
+        let alertController = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        alertController.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(alertController, animated: true)
     }
 }
